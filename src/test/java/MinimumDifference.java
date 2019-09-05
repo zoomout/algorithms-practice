a0d5df80102cf818ff4a7c9ad7a9b9b545bab971
zoomout <bzastavnyi@gmail.com>
2019-09-05 16:34:17 +0200

    cleanup

--- src/test/java/MinimumDifference.java
+++ src/test/java/MinimumDifference.java
@@ -1,8 +1,5 @@
 import org.junit.Test;
 
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static junit.framework.TestCase.assertEquals;
